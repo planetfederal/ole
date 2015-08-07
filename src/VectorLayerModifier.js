dd45198a20ba0bff65d8925dd5faa1b73804534b
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-08-07 15:09:02 +0200

    Add support for transparency

--- src/VectorLayerModifier.js
+++ src/VectorLayerModifier.js
@@ -4,6 +4,10 @@ import StyleGenerator from './StyleGenerator';
 export default class VectorLayerModifier {
   static modifyLayer(layerInfo, layer, mapProjection) {
     var styleGenerator = new StyleGenerator();
+    var transparency = layerInfo.drawingInfo.transparency;
+    if (transparency !== null || transparency !== undefined) {
+      layer.setOpacity((100-transparency)/100);
+    }
     layer.setStyle(styleGenerator.generateStyle(layerInfo.drawingInfo));
     var dpi = 25.4 / 0.28;
     var mpu = ol.proj.METERS_PER_UNIT[mapProjection.getUnits()];
