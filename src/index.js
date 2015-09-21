b3a7077bfaf9a946dec01f5f8c99f96b73afc635
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-09-21 15:17:02 +0200

    Bump to 0.4.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 ** Olé - Integration of OpenLayers 3 and Esri ArcGIS REST services
 ** Copyright 2015 Boundless Spatial, Inc.
 ** License: https://raw.githubusercontent.com/boundlessgeo/ole/master/LICENSE
-** Version: v0.3.0
+** Version: v0.4.0
 */
 import LayerGenerator from './LayerGenerator';
 import StyleGenerator from './StyleGenerator';
